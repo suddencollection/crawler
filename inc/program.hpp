c90d07092574f44c621f026654cf939e1a7fbde1
Nyne <175340361+nynepebbles@users.noreply.github.com>
2025-07-22 13:44:18 -0300

    tests

--- inc/program.hpp
+++ inc/program.hpp
@@ -4,12 +4,16 @@
 #include <vector>
 //
 #include <lexbor/dom/interfaces/node.h>
+#include <curl/curl.h>
+#include <lexbor/dom/interfaces/element.h>
+#include <lexbor/html/interface.h>
+#include <lexbor/html/parser.h>
 
 class PageNode
 {
 public:
   PageNode(std::string url) :
-    url{url}, links{}
+    m_url{url}, m_links{}
   {
   }
 
@@ -17,30 +21,41 @@ public:
   {
   }
 
-  void add(PageNode node) { links.push_back(node); }
-  void reserve(std::size_t size) { links.reserve(size); };
-  auto children() -> std::vector<PageNode> const& { return links; }
+  void add(PageNode node) { m_links.push_back(node); }
+  void reserve(std::size_t size) { m_links.reserve(size); };
+  auto children() -> std::vector<PageNode> const& { return m_links; }
+  auto url() -> std::string const& { return m_url; }
 
 private:
-  std::string url;
-  std::vector<PageNode> links;
+  std::string m_url;
+  std::vector<PageNode> m_links;
 };
 
 class Program
 {
-  using ActualUrl_Content_Pair = std::pair<std::string, std::string>;
+  using FinalURL = std::string;
+  using PageContent = std::string;
+
 public:
+  using Response = std::pair<FinalURL, PageContent>;
+
   Program();
   ~Program();
   void run();
 
-private:
-  auto static write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) -> size_t;
-  void static extract_links_rec(lxb_dom_node_t* node, std::vector<PageNode>& out, std::string const&);
-  auto request_html(std::string const& url) -> ActualUrl_Content_Pair;
-  auto crawl_page(std::string const& url, int max_depth) -> PageNode;
-  void print_header();
-  auto request_input() -> std::string;
-  int request_depth();
+  // flow
+  void static print_header();
+  auto static request_input() -> std::string;
+  auto static request_depth() -> int;
+  auto request_html(std::string const& url) -> Response;
   auto parse_url(std::string url, std::string const& content) -> std::vector<PageNode>;
+
+  // helpers
+  auto static write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) -> size_t; // used by request_html
+  void static extract_links_rec(lxb_dom_node_t* node, std::vector<PageNode>& out, std::string const&);
+  auto static crawl_page(std::string const& url, int max_depth) -> PageNode;
+  bool static is_valid_url(std::string url);
+
+private:
+  CURLM* multi_handle = nullptr;
 };
